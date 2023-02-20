cba1f9d987e26c4453ac62280216fa40c6a2d489
Sorin Barbu <sbarbu38@yahoo.com>
2023-02-20 20:53:45 +0200

    Fixed socket problem, modified index.html

--- server.py
+++ server.py
@@ -6,9 +6,9 @@ import os.path
 HOST = 'localhost'
 PORT = 8000
 
-HTML_400 = (False, {"error": True, "code": 400})
-HTML_404 = (False, {"error": True, "code": 404})
-OK = (False, {"error": False})
+HTML_400 = (False, {"error": 400})
+HTML_404 = (False, {"error": 404})
+OK = (False, {"type": "POST"})
 STOP = (True, {})
 
 DATABASE_PATH = "data.json"
@@ -36,32 +36,39 @@ async def parse_data(data):
         elif data["type"] == "GET":
             if os.path.exists(DATABASE_PATH):
                 database_data = await execute(None, get_data)
-                return (False, {"error": False, "data": database_data})
+                return (False, {"type": "GET", "data": database_data})
             else:
                 return HTML_404
 
     return HTML_400
 
 async def handle_request(websocket, path):
-    execute = asyncio.get_event_loop().run_in_executor
-    await execute(None, print, "CONNECTED")
-    # Wait for a message from the client
-    json_string = await websocket.recv()
+    while True:
+        try:
+            execute = asyncio.get_event_loop().run_in_executor
+            await execute(None, print, "CONNECTED")
+            # Wait for a message from the client
+            json_string = await websocket.recv()
 
-    # Parse the JSON message
-    data = await execute(None, json.loads, json_string)
+            # Parse the JSON message
+            data = await execute(None, json.loads, json_string)
 
-    # Parse the data
-    stop, response = await parse_data(data)
+            # Parse the data
+            stop, response = await parse_data(data)
 
-    if stop == True:
-        # Close the WebSocket connection and stop the server
-        await websocket.close()
-        asyncio.get_event_loop().stop()
-        return
-    
-    # Send the response to the client
-    await websocket.send(json.dumps(response))
+            if stop == True:
+                # Close the WebSocket connection and stop the server
+                await websocket.close()
+                asyncio.get_event_loop().stop()
+                return
+            
+            # Send the response to the client
+            await websocket.send(json.dumps(response))
+        except websockets.exceptions.ConnectionClosed:
+            print("closed")
+            await websocket.close()
+            break
+        
 
 def main():
     # Start the websocket server
