311fee8025cb4a83cec2ee152370c73f95d3a8f3
Sorin Barbu <sbarbu38@yahoo.com>
2023-02-22 19:38:17 +0200

    Finished sidebar logic

--- js/script.js
+++ js/script.js
@@ -30,6 +30,11 @@ let plans = [ {
 // View
 function renderSidebar() {
     const plansDiv = document.getElementById('plans-div');
+    const buttonsDiv = document.getElementById('plans-buttons-div');
+
+    plansDiv.innerHTML = '';
+    buttonsDiv.innerHTML = '';
+
     let button;
     for (let i = 0; i < plans.length; i++) {
         button = document.createElement('button');
@@ -39,30 +44,65 @@ function renderSidebar() {
         button.addEventListener('click', planClick);
         plansDiv.appendChild(button);
     }
+
+    const addPlanButton = document.createElement('input');
+    addPlanButton.id = 'add-plan-button';
+    addPlanButton.type = 'image';
+    addPlanButton.src = 'icons/plus2.png';
+    addPlanButton.alt = 'Add';
+    addPlanButton.addEventListener('click', addPlanClick);
+    buttonsDiv.appendChild(addPlanButton);
+
 };
-function renderMain(index) {
-    console.log(index);
-}
 function renderAddPlan() {
     const plansDiv = document.getElementById('plans-div');
     const textInput = document.createElement('input');
     textInput.type = 'text';
-
+    textInput.id = 'add-plan-input';
     plansDiv.appendChild(textInput);
-}
+    textInput.focus();
 
+    const plansButtonDiv = document.getElementById('plans-buttons-div');
+    const okButton = document.createElement('button');
+    okButton.innerText = 'OK';
+    okButton.addEventListener('click', okPlanClick);
+    const cancelButton = document.createElement('button');
+    cancelButton.innerText = 'CANCEL';
+    cancelButton.addEventListener('click', cancelPlanClick);
+
+    plansButtonDiv.innerHTML = '';
+    plansButtonDiv.appendChild(okButton);
+    plansButtonDiv.appendChild(cancelButton);
+
+};
+function renderMain(index) {
+    console.log(index);
+};
 // Controler
 function planClick(event) {
     const planButton = event.target;
     renderMain(planButton.dataset.planId);
 };
-
 function addPlanClick(event) {
     renderAddPlan();
+};
+function okPlanClick(event) {
+    const textInput = document.getElementById('add-plan-input');
+    if (isValid(textInput.value)) {
+        plans.push({
+            title: textInput.value,
+            goals: []
+        });
+        renderSidebar();
+    }
+};
+function cancelPlanClick(event) {
+    renderSidebar();
+};
+
+
+function isValid(str) {
+    return str.match(/^[a-zA-Z0-9]+$/) !== null && str.length < 16;
 }
-
-
 // Run
 renderSidebar();
-const addPlanButton = document.getElementById('add-plan-button');
-addPlanButton.addEventListener('click', addPlanClick);
